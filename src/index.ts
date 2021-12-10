214c92bc6524d3c2e077552385054c4403954695
Jason <git-admin@casjaysdev.com>
2021-12-09 20:46:40 -0500

    ❓ Added: cors  ❓

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import express, { Request, Response, NextFunction } from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import corsOptions from './middleware/cors';
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors(corsOptions));
+app.use(morgan('combined'));
 
 app.use('/', require('./routes'));
 
---
+++ src/middleware/cors.ts
@@ -0,0 +1,18 @@
+import * as cors from 'cors';
+
+const corsOptions: cors.CorsOptions = {
+  allowedHeaders: [
+    'Origin',
+    'X-Requested-With',
+    'Content-Type',
+    'Accept',
+    'X-Access-Token',
+  ],
+  credentials: true,
+  methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  origin: '*',
+};
+
+export default corsOptions;
